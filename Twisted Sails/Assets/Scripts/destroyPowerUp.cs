cef12b8fe1ee706eb52248a79c85e4a502a23ac8
ohiader <ryan.black@ryblack.com>
2016-09-07 12:03:33 -0700

    Added Power Ups that Randomly Spawn
    Randomy selects from an array of spawn points and an array of power ups
    to spawn a power up every x (currently 45) seconds and lasts x
    (currently 90) seconds. Power ups currently don't do anything to the
    boat as there's no movement system or damage system for them to augement
    at the time of creation.

---
+++ Twisted Sails/Assets/Scripts/destroyPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyPowerUp : MonoBehaviour
+{
+
+    public float destroyTime = 90.0f;
+    public float powerUpRotateSpeed = 120.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * powerUpRotateSpeed);
+    }
+}
---
+++ Twisted Sails/Assets/Scripts/spawnPowerUps.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnPowerUps : MonoBehaviour {
+
+    public Transform[] PowerUpSpawnPoints;
+    public float powerUpSpawnTime = 45.0f;
+
+
+    public GameObject[] PowerUps;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnPowerUps", powerUpSpawnTime, powerUpSpawnTime);
+	}
+
+    void SpawnPowerUps()
+    {
+        int spawnIndex = Random.Range(0, PowerUpSpawnPoints.Length);
+        int objectIndex = Random.Range(0, PowerUps.Length);
+
+        Instantiate(PowerUps[objectIndex], PowerUpSpawnPoints[spawnIndex].position, PowerUpSpawnPoints[spawnIndex].rotation);
+    }
+}
