acf73fd6b9e2f5a94c8f79a6de6c30c92a821e36
cwarren131 <cawarre6@asu.edu>
2016-09-16 15:30:03 -0700

    Buttons
    Connected code to buttons

--- Twisted Sails/Assets/Scripts/StatsUI.cs
+++ Twisted Sails/Assets/Scripts/StatsUI.cs
@@ -23,6 +23,12 @@ using System.Collections;
 
 public class StatsUI : MonoBehaviour {
 
+	//Player Stats
+	public int speed;
+	public int fireRate;
+	public int durability;
+	public double waitTime = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -30,6 +36,44 @@ public class StatsUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		/*if(SpeedButtonPressed){
+		 * 	SpeedButton(speed);
+		 * }
+		*/
+
+		/*if(FireRateButtonPressed){
+		* 	FireRateButton(fireRate);
+		* }
+		*/
+
+		/*if(DurabilityButtonPressed){
+		* 	DurabilityButton(durability);
+		* }
+		*/
 	}
+
+	public void SpeedButton(int baseSpeed){
+		double coolDown = 0.0f;
+		if(Time.deltaTime > coolDown){
+			//ChangeSpeed(baseSpeed)
+			coolDown = Time.deltaTime + waitTime;
+		}
+	}
+
+	public void FireRateButton(int baseFireRate){
+		double coolDown = 0.0f;
+		if(Time.deltaTime > coolDown){
+			//ChangeFireRate(baseFireRate)
+			coolDown = Time.deltaTime + waitTime;
+		}
+	}
+
+	public void DurabilityButton(int baseDurability){
+		double coolDown = 0.0f;
+		if(Time.deltaTime > coolDown){
+			//ChangeDurability(baseDurability)
+			coolDown = Time.deltaTime + waitTime;
+		}
+	}
+
 }
