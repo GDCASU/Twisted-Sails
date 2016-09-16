717dcd7586fbecd971661ad02b8783f69ea9ef0f
cwarren131 <cawarre6@asu.edu>
2016-09-16 13:56:46 -0700

    StatUI
    Psuedocode

---
+++ Twisted Sails/Assets/Scripts/StatsUI.cs
@@ -0,0 +1,35 @@
+﻿/*
+ WaitTime = 5.0f
+ 
+ SpeedButton(baseSpeed){
+ 	if(Time > CoolDown)
+ 		ChangeSpeed(baseSpeed)
+		CoolDown = Time + WaitTime
+ }
+ FireRateButton(baseFireRate){
+ 	if(Time > CoolDown)
+ 		ChangeFireRate(baseFireRate)
+ 		CoolDown = Time + WaitTime
+ }
+ DurabilityButton(baseDurability){
+ 	if(Time > CoolDown)
+ 		ChangeDurability(baseDurability)
+ 		CoolDown = Time + WaitTime
+ }
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class StatsUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
