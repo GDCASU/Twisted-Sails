bb23acd5b0db85f1c1810928688f1b56dd18bdf9
CoryStory <cwyman1@asu.edu>
2016-09-29 21:22:44 -0700

    Wyman Combat Functionality
    Created a swivel cannon on front end, also created an active state
    system. Pushing 1 toggles which weapon is active, so that when the
    swivel cannon is active one can rotate the cannon but will not move the
    boat in doing so

---
+++ Twisted Sails/Assets/Scripts/Weapon Scripts/Active Weapon States/ActiveRingScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveRingScript : MonoBehaviour {
+
+	public Transform cannon;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = cannon.position;
+	}
+}
