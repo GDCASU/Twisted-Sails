750645bcb1a848ef98ed9b702df0da62cc392be6
Keter <sum30n3@yahoo.com>
2016-10-26 11:17:44 -0700

    Networked health bars

---
+++ Twisted Sails/Assets/Scripts/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//The only purpose of this script is to have a component always facing the camera
+//Currently only used to have health bars above other ships be always visible
+public class Billboard : MonoBehaviour
+{
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+}
