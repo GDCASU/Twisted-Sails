ca4b73139c8b727212443e24f1503c44afbfa57b
S. Alex Bradt <jai7breaker@gmail.com>
2016-11-15 18:47:40 -0700

    MotionLogger.

---
+++ Twisted Sails/Assets/Scripts/MotionLogger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionLogger : MonoBehaviour
+{
+    public float m_TimeBetweenLogs;
+    private Rigidbody m_Body;
+    private float m_LastLog;
+
+    void Start()
+    {
+        m_Body = GetComponent<Rigidbody>();
+        m_LastLog = Time.time;
+	}
+	
+	void Update()
+    {
+        if (Time.time - m_LastLog > m_TimeBetweenLogs)
+        {
+            Debug.Log("Velocity: " + m_Body.velocity);
+            m_LastLog = Time.time;
+        }
+	}
+}
