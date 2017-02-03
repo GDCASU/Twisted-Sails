d7d160a4ad9b3706a0c75b9c2438a03cc8f128a7
Kyle Chapman <xeuorux@gmail.com>
2017-02-02 21:47:15 -0700

    First version of status effects system

---
+++ Twisted Sails/Assets/Scripts/Status Effect/StatusEffect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class StatusEffect
+{
+	private float remainingDuration;
+
+	public float GetDuration()
+	{
+		return -1f;
+	}
+
+	public bool GetIsFinished()
+	{
+		return remainingDuration <= 0;
+	}
+
+	public int GetMaxStacks()
+	{
+		return 1;
+	}
+
+	public bool DoesOverrideExisting()
+	{
+		return false;
+	}
+
+	public void StartEffect(GameObject playerBoat)
+	{
+		remainingDuration = GetDuration();
+    }
+
+	public void UpdateEffect(GameObject playerBoat)
+	{
+		remainingDuration -= Time.deltaTime;
+	}
+
+	public void EndEffect(GameObject playerBoat)
+	{
+
+	}
+}
---
+++ Twisted Sails/Assets/Scripts/Status Effect/StatusEffectsManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class StatusEffectsManager : MonoBehaviour
+{
+	public readonly List<StatusEffect> activeEffects = new List<StatusEffect>();
+	private GameObject playerBoat;
+
+	private void Start()
+	{
+		playerBoat = this.gameObject;
+    }
+
+	private void Update()
+	{
+		for(int i = 0; i< activeEffects.Count; i++)
+		{
+			activeEffects[i].UpdateEffect(playerBoat);
+			if (activeEffects[i].GetIsFinished())
+			{
+				activeEffects.RemoveAt(i);
+				i--;
+			}
+		}
+	}
+
+	public void AddNewEffect(StatusEffect effect)
+	{
+		int currentStacksActive;
+		if (!IsEffectTypeInList<StatusEffect>(out currentStacksActive) || currentStacksActive < effect.GetMaxStacks() || effect.DoesOverrideExisting())
+		{
+			if (effect.DoesOverrideExisting())
+			{
+                RemoveSameTypeEffectsFromList(effect);
+            }
+			effect.StartEffect(playerBoat);
+			activeEffects.Add(effect);
+        }
+    }
+
+	public bool IsEffectTypeInList<T>(out int amount) where T : StatusEffect
+	{
+		amount = 0;
+        foreach (StatusEffect e in activeEffects)
+		{
+			if (e is T)
+			{
+				amount++;
+            }
+		}
+		return amount > 0;
+	}
+
+	public bool IsSameTypeEffectInList(StatusEffect effect, out int amount)
+	{
+		Type effectsType = effect.GetType();
+		amount = 0;
+		foreach (StatusEffect e in activeEffects)
+		{
+			if (e.GetType() == effectsType)
+			{
+				amount++;
+			}
+		}
+		return amount > 0;
+	}
+
+	public T RemoveEffectOfTypeFromList<T>() where T : StatusEffect
+	{
+		for (int i = 0; i < activeEffects.Count;i++)
+		{
+			if (activeEffects[i] is T)
+			{
+				activeEffects.RemoveAt(i);
+			}
+		}
+		return default(T);
+	}
+
+	public StatusEffect RemoveSameTypeEffectsFromList(StatusEffect effect)
+	{
+		Type effectsType = effect.GetType();
+		for (int i = 0; i < activeEffects.Count; i++)
+		{
+			if (activeEffects[i].GetType() == effectsType.GetType())
+			{
+				activeEffects[i].EndEffect(playerBoat);
+				activeEffects.RemoveAt(i);
+				i--;
+			}
+		}
+		return null;
+	}
+}
