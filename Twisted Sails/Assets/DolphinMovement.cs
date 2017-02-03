ff529f3b5bb43b421cfadbc6a911ff8242450bea
jayseanbrizzy <jabrisbin@gmail.com>
2017-02-03 15:57:22 -0700

    Base Dolphin
    The base implementation of the dolphi object.
    Just sits there currently.

---
+++ Twisted Sails/Assets/DolphinMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DolphinMovement : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
