c159bb9b89e2360587205ffb2e729afb3825cbfa
Raptorhands <jcash1@asu.edu>
2017-02-04 20:17:11 -0700

    Base Boomarang Functionality
    >Added BrambleProjectileBehavior Script
    -Utilized the Vector3.MoveTowards method at run time to go to a target
    postion (saved at instantiantion).
    -Uses .Ivoke to set a boolean check to false, allowing update to switch
    to the same method in reverse.
    >Networking Issues Persist

---
+++ Twisted Sails/Assets/BrambleProjectileBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrambleProjectileBehavior : MonoBehaviour {
+
+	public float speed = 1f;
+	public float travelTime = 8f;
+	Vector3 brambleTarget;
+	Vector3 brambleStart;
+	bool goingOut = true;
+
+	// Use this for initialization
+	void Start () {
+		this.Invoke ("ReturnToShipPos", travelTime);
+		brambleStart = GameObject.Find("BrambleShipPlayer(Clone)").transform.position;
+		brambleTarget = GameObject.Find("BrambleShipPlayer(Clone)/HWtarget").transform.position;
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(goingOut) 
+			transform.position = Vector3.MoveTowards (this.transform.position, brambleTarget, speed * Time.deltaTime);
+		else
+			transform.position = Vector3.MoveTowards (this.transform.position, brambleStart, speed * Time.deltaTime);
+	}
+	void ReturnToShipPos(){
+		goingOut = false;
+	}
+}
+	
