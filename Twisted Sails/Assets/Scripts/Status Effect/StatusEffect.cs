ca6e97c70b6caf69e136b61995fe185bfdb1e56d
Kyle Chapman <xeuorux@gmail.com>
2017-02-06 15:17:21 -0700

    PhysicsHelper first new vector math method, status effects system improvements and comments

--- Twisted Sails/Assets/Scripts/Status Effect/StatusEffect.cs
+++ Twisted Sails/Assets/Scripts/Status Effect/StatusEffect.cs
@@ -1,42 +1,98 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//  Programmer:     Kyle Chapman
+//  Date:           2-06-17
+//  Description:    
+//		An abstract parent class for status effect classes to derive from for use in the status effects system
+//		managed by an instance of the StatusEffectsManager class on a particular boat.
+
 public abstract class StatusEffect
 {
 	private float remainingDuration;
+	private bool isFinished;
 
-	public float GetDuration()
+	/// <summary>
+	/// Get the starting duration in seconds that the effect will last on the boat.
+	/// By default the duration is -1. Negative values cause the effect to never expire.
+	/// Should be overridden to any nonzero value.
+	/// </summary>
+	/// <returns>The starting duration in seconds that the effect will last on the boat.</returns>
+	public float GetStartingDuration()
 	{
 		return -1f;
 	}
 
+	/// <summary>
+	/// Get if the status effect believes itself to be expired.
+	/// By default, returns true if the remaining duration went from 0 or above to less than zero.
+	/// </summary>
+	/// <returns>Whether the status effect is expired or not.</returns>
 	public bool GetIsFinished()
 	{
-		return remainingDuration <= 0;
+		return isFinished;
 	}
 
+	/// <summary>
+	/// Get the max number of stacks of this type that are allowed to exist at one time on a boat.
+	/// The default value is 1, and it should be overridden to any value 1 or greater.
+	/// </summary>
+	/// <returns>The max number of stacks of this type that are allowed to exist at one time on a boat.</returns>
 	public int GetMaxStacks()
 	{
 		return 1;
 	}
 
+	/// <summary>
+	/// Get if the status effect overrides existing instances of the effect on the boat when it is applied.
+	/// If no, a new instance of the effect will not be applied if the maximum amount of stacks of the effect are already on the boat.
+	/// If yes, no matter how many stacks of the effect are currently on the boat, all of them will be removed when a new instance is put on.
+	/// Returns false by default.
+	/// </summary>
+	/// <returns></returns>
 	public bool DoesOverrideExisting()
 	{
 		return false;
 	}
 
+	/// <summary>
+	/// Tells the status effect to begin its effect on the boat.
+	/// This is where the code that begins the effect is put, with reversing code on the end effect method.
+	/// Also by default starts the duration timer.
+	/// </summary>
+	/// <param name="playerBoat">The instance of the boat onto which this effect is being applied.</param>
 	public void StartEffect(GameObject playerBoat)
 	{
-		remainingDuration = GetDuration();
+		remainingDuration = GetStartingDuration();
+		isFinished = false;
     }
 
+	/// <summary>
+	/// Updates the effect.
+	/// By default, updates the duration timer and determins if the effect has expired.
+	/// </summary>
+	/// <param name="playerBoat">The instance of the boat onto which this effect is being applied.</param>
 	public void UpdateEffect(GameObject playerBoat)
 	{
-		remainingDuration -= Time.deltaTime;
+		if (remainingDuration >= 0)
+		{
+			remainingDuration -= Time.deltaTime;
+			if (remainingDuration <= 0)
+			{
+				isFinished = true;
+			}
+		}
+		
 	}
 
+	/// <summary>
+	/// Tells the status effect to end its effect on the boat.
+	/// This is where the code that ends the effect is put, reversing the starting code on the start effect method.\
+	/// By default, does nothing.
+	/// </summary>
+	/// <param name="playerBoat">The instance of the boat onto which this effect is being applied.</param>
 	public void EndEffect(GameObject playerBoat)
 	{
-
+		
 	}
 }
--- Twisted Sails/Assets/Scripts/Status Effect/StatusEffectsManager.cs
+++ Twisted Sails/Assets/Scripts/Status Effect/StatusEffectsManager.cs
@@ -3,9 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
+//  Programmer:     Kyle Chapman
+//  Date:           2-06-17
+//  Description:    Manages a list of and the condition of Status Effects on a boat.
+
 public class StatusEffectsManager : MonoBehaviour
 {
+	//the current list of effects active on the boat
 	public readonly List<StatusEffect> activeEffects = new List<StatusEffect>();
+
+	//the boat to which the effects are applied
 	private GameObject playerBoat;
 
 	private void Start()
@@ -15,20 +22,35 @@ public class StatusEffectsManager : MonoBehaviour
 
 	private void Update()
 	{
+		//for each effect, give it an update tick
+		//then ask if it is complete, if it is, tell it to end its effect
+		//and remove it from the list
 		for(int i = 0; i< activeEffects.Count; i++)
 		{
 			activeEffects[i].UpdateEffect(playerBoat);
 			if (activeEffects[i].GetIsFinished())
 			{
+				activeEffects[i].EndEffect(playerBoat);
 				activeEffects.RemoveAt(i);
 				i--;
 			}
 		}
 	}
 
+	/// <summary>
+	/// Add a new status effect to the list of active effects.
+	/// Managing for stacking properties, override properties
+	/// Then tells the effect to start itself.
+	/// </summary>
+	/// <param name="effect">The effect obect to begin on the boat.</param>
 	public void AddNewEffect(StatusEffect effect)
 	{
 		int currentStacksActive;
+
+		//if the effect is not currently in the list
+		//or if the effect has less than its maximum allowed amount of stacks in the list
+		//or if the new effect always overrides existing effects no matter what
+		//add the effect to the list
 		if (!IsEffectTypeInList<StatusEffect>(out currentStacksActive) || currentStacksActive < effect.GetMaxStacks() || effect.DoesOverrideExisting())
 		{
 			if (effect.DoesOverrideExisting())
@@ -40,6 +62,14 @@ public class StatusEffectsManager : MonoBehaviour
         }
     }
 
+	/// <summary>
+	/// Determines if an effect of the given genericized type is in the list.
+	/// If it is, it returns true and assigns the out parameter amount to the
+	/// number of effects from of that type that are in the list.
+	/// </summary>
+	/// <typeparam name="T">The type of the status effect subclass to look for the in the list.</typeparam>
+	/// <param name="amount">The out parameter to assign the number of objects found of the desired type.</param>
+	/// <returns>True if at least one matching object was found, false otherwise.</returns>
 	public bool IsEffectTypeInList<T>(out int amount) where T : StatusEffect
 	{
 		amount = 0;
@@ -53,6 +83,14 @@ public class StatusEffectsManager : MonoBehaviour
 		return amount > 0;
 	}
 
+	/// <summary>
+	/// Determines if an effect that has the same type as the given object is in the list.
+	/// If it is, it returns true and assigns the out parameter amount to the
+	/// number of effects from of that type that are in the list.
+	/// </summary>
+	/// <param name="effect">The StatusEffect object whose subclass type is to be looked for in the list.</param>
+	/// <param name="amount">The out parameter to assign the number of objects found of the desired type.</param>
+	/// <returns>True if at least one matching object was found, false otherwise.</returns>
 	public bool IsSameTypeEffectInList(StatusEffect effect, out int amount)
 	{
 		Type effectsType = effect.GetType();
@@ -67,7 +105,12 @@ public class StatusEffectsManager : MonoBehaviour
 		return amount > 0;
 	}
 
-	public T RemoveEffectOfTypeFromList<T>() where T : StatusEffect
+	/// <summary>
+	/// Removes all status effect objects from the list that match the given genericed type
+	/// which is a subclass type of Status Effect.
+	/// </summary>
+	/// <typeparam name="T">The type of the status effect subclass whose objects are to be removed from the list.</typeparam>
+	public void RemoveEffectOfTypeFromList<T>() where T : StatusEffect
 	{
 		for (int i = 0; i < activeEffects.Count;i++)
 		{
@@ -76,10 +119,14 @@ public class StatusEffectsManager : MonoBehaviour
 				activeEffects.RemoveAt(i);
 			}
 		}
-		return default(T);
 	}
 
-	public StatusEffect RemoveSameTypeEffectsFromList(StatusEffect effect)
+	/// <summary>
+	/// Removes all status effect objects from the list whose type is the
+	/// same as the type of the given status effect object.
+	/// </summary>
+	/// <param name="effect">The object that is to be matched to find objects in the list to remove.</param>
+	public void RemoveSameTypeEffectsFromList(StatusEffect effect)
 	{
 		Type effectsType = effect.GetType();
 		for (int i = 0; i < activeEffects.Count; i++)
@@ -91,6 +138,5 @@ public class StatusEffectsManager : MonoBehaviour
 				i--;
 			}
 		}
-		return null;
 	}
 }
