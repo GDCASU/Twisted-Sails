1b4a7bf9a379db5136631a62b00b8e0f0e34aa02
jayseanbrizzy <jabrisbin@gmail.com>
2017-02-10 16:05:41 -0700

    Got the dolphin to move kind of

---
+++ Twisted Sails/Assets/AutoMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMovement : MonoBehaviour {
+
+	// Instance Data:
+	public double speed;
+	public double turnFrequency;
+	public Rigidbody rb;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public static void randomMove(){
+	
+		rb = this.GetComponent<Rigidbody> ();
+
+
+
+	}
+
+}
--- Twisted Sails/Assets/DolphinMovement.cs
+++ Twisted Sails/Assets/DolphinMovement.cs
@@ -4,16 +4,48 @@ using UnityEngine;
 
 public class DolphinMovement : MonoBehaviour {
 
-	public int speed;
+	// Instance Data:
+	public float speed = 10;
+	public float turnFrequency;
+	public float jumpSpeed = 10;
+	private Rigidbody rb;
+
+	public int timerMax;
+	private int currentTime;
 
 	// Use this for initialization
 	void Start () {
-		speed = 0;
+		
+		currentTime = timerMax;
+
+
+
+
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		this.move ();
+		currentTime -= Time.deltaTime;
+		if (currentTime % 2 == 0) {
+			rb.AddForce (Vector3.up * jumpSpeed);
+			//currentTime = timerMax;
+		}
+
+		/*if (currentTime % 4 == 0) {
+			rb.AddRelativeTorque(Vector3.right);	
+		}*/
+	}
+
+	public void move(){
+	
+		rb = this.GetComponent<Rigidbody> ();
+		rb.AddForce (transform.forward * speed * Time.deltaTime);
+
+
+
+
 	}
 }
