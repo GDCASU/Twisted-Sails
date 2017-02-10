131be6d5eb95a6a00b1ae830ad642cd7949de3cb
Raptorhands <jcash1@asu.edu>
2017-02-09 19:34:21 -0700

    Sunship Heavy Weapon BASE functionality
    #Joseph Cash#
    Sunship now shoots a projectile in the forward direction (relative).
    The projectile checks the distance between its fired point and its
    current location, once the (modifiable) threshhold has been met: it will
    destroy its self.
    
    Further work needs to be done to create the AOE effect. We need to talk
    to programming on how to make this happen.

--- Twisted Sails/Assets/Scripts/SunshipHeavyProjectileBeahvior.cs
+++ Twisted Sails/Assets/Scripts/SunshipHeavyProjectileBeahvior.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class SunshipHeavyProjectileBeahvior : MonoBehaviour {
 
-	Transform startingLocation;
-	Transform currentLocation;
+	Vector3 startingLocation;
+	Vector3 currentLocation;
 	float distanceFromOrigin;
 	public float maxDistance =1f;
 
 
 	// Use this for initialization
 	void Start () {
-		startingLocation = this.transform; 
-		Debug.Log("My starting location was:" + startingLocation.position);
+		startingLocation = this.transform.position; 
+		Debug.Log("My starting location was:" + startingLocation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentLocation = this.transform;
-		Debug.Log ("current location is" + currentLocation.position);
-		distanceFromOrigin = Mathf.Abs(Vector3.Distance(currentLocation.position, startingLocation.position));
+		currentLocation = this.transform.position;
+		//Debug.Log ("current location is" + currentLocation);
+		distanceFromOrigin = Mathf.Abs(Vector3.Distance(currentLocation, startingLocation));
 		Debug.Log (distanceFromOrigin);
 		if (distanceFromOrigin >= maxDistance) {
 			Debug.Log ("I have hit my terminal location, please blow me up");
+			Destroy (this.gameObject); 
 		}
 
 		
