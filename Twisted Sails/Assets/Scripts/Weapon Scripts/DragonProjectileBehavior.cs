04fb1f15d41a423a79044e95a69b20bc379f80a5
Sean McGonegle <smcgonegle@gmail.com>
2017-02-24 11:38:28 -0700

    Dragon Heavy Weapon Polish
    Made it nice and shiny

---
+++ Twisted Sails/Assets/Scripts/Weapon Scripts/DragonProjectileBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonProjectileBehavior : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Dragon Projectile: Collide");
+
+        if (other.gameObject.tag == "Player")
+            Debug.Log("Bramble Projectile: I Hit a player");
+
+        Destroy (this.gameObject);
+    }
+}
