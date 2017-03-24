8916e6e28475ebb3cf090cb93d07e7cbb5144efa
EricEZ <ezuk101@gmail.com>
2017-03-24 13:51:47 -0700

    Adding spawner scripts and prefabs with resource packs
    Created the spawner and prefab for the heavy weapon ammo pack  althought
    there is an error with the ammo pack no spawning in the right place.

---
+++ Twisted Sails/Assets/Scripts/Resource Packs/HeavyAmmoPackSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyAmmoPackSpawner : MonoBehaviour
+{
+    public GameObject ammoPack;
+    private GameObject createdPack;
+    public float timer;
+    public float timerLength;
+    private bool spawnedPack = false;
+    private void Start()
+    {
+       timer = timerLength;
+    }
+    private void Update()
+    {
+        if (spawnedPack == false)
+        {
+            if (timer <= 0)
+            {
+                createdPack=Instantiate(ammoPack,Vector3.zero, transform.rotation);
+                createdPack.transform = transform;
+                spawnedPack = true;
+            }
+            else
+                timer -= Time.deltaTime;
+        }
+        else
+        {
+            if(createdPack==null)
+            {
+                timer = timerLength;
+                spawnedPack = false;
+            }
+        }
+    }
+
+
+
+
+
+
+}
---
+++ Twisted Sails/Assets/Scripts/Resource Packs/HeavyWeaponPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyWeaponPickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(collision.gameObject.GetComponent<HeavyWeapon>().AmmoCount<= collision.gameObject.GetComponent<HeavyWeapon>().ammoCapacity)
+            collision.gameObject.GetComponent<HeavyWeapon>().AddAmmo(1);
+            Destroy(gameObject);
+        }
+
+
+    }
+
+}
