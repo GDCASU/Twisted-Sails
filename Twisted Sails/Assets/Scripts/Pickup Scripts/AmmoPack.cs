ba9982ba9a123378fb73c7d0df0adc49a169bdea
Sean McGonegle <smcgonegle@gmail.com>
2017-04-14 02:06:58 -0700

    Ammo/Health Pack Updates
    I moved the art assets for the HealthPack to a different folder, hence
    the red.
    
    Prefabs:
    - AmmoPackFix
    - AmmoSpawnerFix
    - HealthPackSpawnerFix
    - HealthPackFix
    
    New Scripts:
    - HealthSpawnerFix: Fixes the endless spawning glitch, by checking to
    see if there is already a pack spawned
    - AmmoSpawner*: Same as health
    - AmmoPack*:  Old script accidently gave player health
    
    New Scene: MainLevelSean
    New AmmoPack art assets added
    
    Oh I also added an AmmoPickUp tag. I dunno if it's important at all, but
    the HealthPack had one, so you know...
    
    *The old AmmoSpawner and AmmoPack scripts are called
    HeavyAmmoPackSpawner, and HeavyWeaponPickup respectively

---
+++ Twisted Sails/Assets/Scripts/Pickup Scripts/AmmoPack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class AmmoPack : InteractiveObject
+{
+    public int ammoAmmount = 1;
+    public MeshRenderer packMesh;
+    public Collider packCollider;
+
+    private void Start()
+    {
+        if (packMesh == null)
+        {
+            packMesh = GetComponent<MeshRenderer>();
+            packCollider = GetComponent<Collider>();
+        }
+    }
+
+    public override void OnInteractWithPlayer(Health playerHealth, GameObject playerBoat, StatusEffectsManager manager, Collision collision)
+    {
+        //notifies the player events system that the player who interacted with this object picked up a health pack (this object)
+        //also sets isHealthPack to true, since this is a health pack
+        Player.ActivateEventPlayerPickup(MultiplayerManager.FindPlayer(playerBoat.GetComponent<NetworkIdentity>().netId), true);
+
+        //send out the command to change the players health
+        //setting the source of the healthpack to nothing, since no player is responsible
+        if (isServer)
+        {
+            playerBoat.GetComponent<HeavyWeapon>().AddAmmo(ammoAmmount);
+            Destroy(gameObject);
+
+        }
+    }
+
+}
