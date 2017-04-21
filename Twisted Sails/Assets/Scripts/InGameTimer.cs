55a771ea67f04ed8d79d6704585e1efff180e0a5
Keter <sum30n3@yahoo.com>
2017-04-21 15:15:43 -0700

    Splash screen, scene merges

--- Twisted Sails/Assets/Scripts/InGameTimer.cs
+++ Twisted Sails/Assets/Scripts/InGameTimer.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InGameTimer : MonoBehaviour {
+public class InGameTimer : MonoBehaviour
+{
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         float time = ((TeamDeathmatch)MultiplayerManager.GetCurrentGamemode()).timeRemaining;
-        int seconds = (int)(time % 60);
-        GetComponent<Text>().text = (int)(time / 60) + ":" + (seconds >= 10 ? seconds.ToString() : ("0" + seconds));
-	}
+        if (time < 0)
+        {
+            GetComponent<Text>().text = "Overtime!";
+        }
+        else {
+            int seconds = (int)(time % 60);
+            GetComponent<Text>().text = (int)(time / 60) + ":" + (seconds >= 10 ? seconds.ToString() : ("0" + seconds));
+        }
+
+    }
 }
