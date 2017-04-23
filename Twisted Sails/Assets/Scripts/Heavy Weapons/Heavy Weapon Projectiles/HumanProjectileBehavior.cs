5787dc92ad1aa997166ef674d44834b83aa703ec
Kyle Chapman <xeuoruxgmail.com>
2017-04-23 15:59:34 -0700

    Major Deletions of unneccesary files

---
+++ Twisted Sails/Assets/Scripts/Heavy Weapons/Heavy Weapon Projectiles/HumanProjectileBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanProjectileBehavior : InteractiveObject {
+
+    /// <summary>
+    /// Collision for the projectile is handled here in a seperate script which is attactched to the 
+    /// projectile itself. Checks to see if collision is with an enemy, which it will then deal damage to.
+    /// Destroys itself after.
+    /// </summary>
+    /// <param name="other"></param>
+
+    StatusEffectsManager manager;
+    public int damageDealt;
+
+
+    public override void OnInteractWithPlayer(Health playerHealth, GameObject playerBoat, StatusEffectsManager manager, Collision collision)
+    {
+        base.OnInteractWithPlayer(playerHealth, playerBoat, manager, collision);
+
+        int healthChange = -damageDealt;
+
+        //if this object is on the side of the player who owns this object
+        //send out the command to change the players health
+        //setting the source of the health change to be the owner of this cannonball
+        playerHealth.ChangeHealth(healthChange, owner);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag != "Player")
+            Destroy(gameObject);
+    }
+}
