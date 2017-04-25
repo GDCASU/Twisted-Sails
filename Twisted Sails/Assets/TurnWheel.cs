55cf7933a5ed118db7db3b8fd1a56d7f48e373aa
Excoh <excel.ortega@gmail.com>
2017-04-24 23:30:23 -0700

    Turn Wheel Bramble Ship
    - Added Turning Wheel Animation for Bramble Ship

---
+++ Twisted Sails/Assets/TurnWheel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnWheel : MonoBehaviour {
+
+    public GameObject movementShip;
+    public int turnDirection;
+    private BoatMovementNetworked movement;
+    private float turnSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+        movement = movementShip.GetComponent<BoatMovementNetworked>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * (turnSpeed * movement.speed) * turnDirection, Space.Self);
+	}
+}
