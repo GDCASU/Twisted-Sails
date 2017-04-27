c78134a3e15d8153e9554aae1b503211119073a5
Keterr <sum30n3@yahoo.com>
2017-04-26 22:22:01 -0700

    Replace splash screen with video

--- Twisted Sails/Assets/Scripts/SplashScreenController.cs
+++ Twisted Sails/Assets/Scripts/SplashScreenController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashScreenController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(ChangeSceneAfterTime());
+        ((MovieTexture)GetComponent<Image>().material.mainTexture).Play();
+        StartCoroutine(ChangeSceneAfterTime(((MovieTexture)GetComponent<Image>().material.mainTexture).duration));
 	}
 	
-	IEnumerator ChangeSceneAfterTime()
+	IEnumerator ChangeSceneAfterTime(float time)
     {
-        yield return new WaitForSeconds(7);
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene("Title Screen");
     }
 }
